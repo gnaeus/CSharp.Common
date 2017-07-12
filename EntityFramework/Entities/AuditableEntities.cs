f532dbd8a334001bb89d9d3304bb8e44ec61b899
PanyushkinD <PanyushkinD@quantumart.ru>
2017-07-12 11:20:23 +0300

    Add EntityFramework extensions for Auditable entities

---
+++ EntityFramework/Entities/AuditableEntities.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace EntityFramework.Common.Entities
+{
+    /// <summary>
+    /// Used to standardize soft deleting entities. Soft-delete entities are not actually deleted,
+    /// marked as IsDeleted = true in the database, but can not be retrieved to the application.
+    /// </summary>
+    public interface ISoftDeletable
+    {
+        bool IsDeleted { get; set; }
+    }
+
+    /// <summary>
+    /// An entity can implement this interface if it should use Optimistic Concurrency Check
+    /// with populating <see cref="RowVersion"/> from client-side.
+    /// </summary>
+    public interface IOptimisticConcurrent
+    {
+        byte[] RowVersion { get; set; }
+    }
+
+    /// <summary>
+    /// An entity can implement this interface if <see cref="CreatedUtc"/> of this entity must be stored.
+    /// <see cref="CreatedUtc"/> is automatically set when saving Entity to database.
+    /// </summary>
+    public interface ICreationTrackable
+    {
+        DateTime CreatedUtc { get; set; }
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities that is wanted
+    /// to store creation information (who and when created).
+    /// Creation time and creator user are automatically set when saving Entity to database.
+    /// </summary>
+    public interface ICreationAuditable<TUserId> : ICreationTrackable
+        where TUserId : struct
+    {
+        TUserId CreatorUserId { get; set; }
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities that is wanted
+    /// to store creation information (who and when created).
+    /// Creation time and creator user are automatically set when saving Entity to database.
+    /// </summary>
+    public interface ICreationAuditable : ICreationTrackable
+    {
+        string CreatorUser { get; set; }
+    }
+
+    /// <summary>
+    /// An entity can implement this interface if <see cref="UpdatedUtc"/> of this entity must be stored.
+    /// <see cref="UpdatedUtc"/> is automatically set when updating Entity.
+    /// </summary>
+    public interface IModificationTrackable
+    {
+        DateTime? UpdatedUtc { get; set; }
+    }
+    
+    /// <summary>
+    /// This interface is implemented by entities that is wanted
+    /// to store modification information (who and when modified lastly).
+    /// Properties are automatically set when updating the Entity.
+    /// </summary>
+    public interface IModificationAuditable<TUserId> : IModificationTrackable
+        where TUserId : struct
+    {
+        TUserId? UpdaterUserId { get; set; }
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities that is wanted
+    /// to store modification information (who and when modified lastly).
+    /// Properties are automatically set when updating the Entity.
+    /// </summary>
+    public interface IModificationAuditable : IModificationTrackable
+    {
+        string UpdaterUser { get; set; }
+    }
+
+    /// <summary>
+    /// An entity can implement this interface if <see cref="DeletedUtc"/> of this entity must be stored.
+    /// <see cref="DeletedUtc"/> is automatically set when deleting Entity.
+    /// </summary>
+    public interface IDeletionTrackable : ISoftDeletable
+    {
+        DateTime? DeletedUtc { get; set; }
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities which wanted
+    /// to store deletion information (who and when deleted).
+    /// </summary>
+    public interface IDeletionAuditable<TUserId> : IDeletionTrackable
+        where TUserId : struct
+    {
+        TUserId? DeleterUserId { get; set; }
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities which wanted
+    /// to store deletion information (who and when deleted).
+    /// </summary>
+    public interface IDeletionAuditable : IDeletionTrackable
+    {
+        string DeleterUser { get; set; }
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities which modification times must be tracked.
+    /// Related properties automatically set when saving/updating/deleting Entity objects.
+    /// </summary>
+    public interface IFullTrackable : ICreationTrackable, IModificationTrackable, IDeletionTrackable
+    {
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities which must be audited.
+    /// Related properties automatically set when saving/updating/deleting Entity objects.
+    /// </summary>
+    public interface IFullAuditable<TUserId> : IFullTrackable,
+        ICreationAuditable<TUserId>, IModificationAuditable<TUserId>, IDeletionAuditable<TUserId>
+        where TUserId : struct
+    {
+    }
+
+    /// <summary>
+    /// This interface is implemented by entities which must be audited.
+    /// Related properties automatically set when saving/updating/deleting Entity objects.
+    /// </summary>
+    public interface IFullAuditable : IFullTrackable,
+        ICreationAuditable, IModificationAuditable, IDeletionAuditable
+    {
+    }
+}
---
+++ EntityFramework/Extensions/AuditableEntitiesExtensions.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using EntityFramework.Common.Entities;
+
+namespace EntityFramework.Common.Extensions
+{
+    public static partial class DbContextExtensions
+    {
+        /// <summary>
+        /// Populate special properties for all Auditable Entities in context.
+        /// </summary>
+        public static void UpdateAuditableEntities<TUserId>(this DbContext context, TUserId editorUserId)
+            where TUserId : struct
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            var entries = context.GetChangedEntries(
+                EntityState.Added | EntityState.Modified | EntityState.Deleted);
+
+            foreach (var entry in entries)
+            {
+                UpdateAuditableEntity(entry, utcNow, editorUserId);
+            }
+        }
+
+        /// <summary>
+        /// Populate special properties for all Auditable Entities in context.
+        /// </summary>
+        public static void UpdateAuditableEntities(this DbContext context, string editorUser)
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            var entries = context.GetChangedEntries(
+                EntityState.Added | EntityState.Modified | EntityState.Deleted);
+
+            foreach (var entry in entries)
+            {
+                UpdateAuditableEntity(entry, utcNow, editorUser);
+            }
+        }
+
+        /// <summary>
+        /// Populate special properties for all Trackable Entities in context.
+        /// </summary>
+        public static void UpdateTrackableEntities(this DbContext context)
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            var entries = context.GetChangedEntries(
+                EntityState.Added | EntityState.Modified | EntityState.Deleted);
+
+            foreach (var entry in entries)
+            {
+                UpdateTrackableEntity(entry, utcNow);
+            }
+        }
+
+        private static void UpdateAuditableEntity<TUserId>(
+            DbEntityEntry entry, DateTime utcNow, TUserId editorUserId)
+            where TUserId : struct
+        {
+            object entity = entry.Entity;
+
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    var creationAuditable = entity as ICreationAuditable<TUserId>;
+                    if (creationAuditable != null)
+                    {
+                        creationAuditable.CreatorUserId = editorUserId;
+                    }
+                    break;
+
+                case EntityState.Modified:
+                    var modificationAuditable = entity as IModificationAuditable<TUserId>;
+                    if (modificationAuditable != null)
+                    {
+                        modificationAuditable.UpdaterUserId = editorUserId;
+                    }
+                    break;
+
+                case EntityState.Deleted:
+                    var deletionAuditable = entity as IDeletionAuditable<TUserId>;
+                    if (deletionAuditable != null)
+                    {
+                        deletionAuditable.DeleterUserId = editorUserId;
+                    }
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+            
+            UpdateTrackableEntity(entry, utcNow);
+        }
+
+        private static void UpdateAuditableEntity(
+            DbEntityEntry entry, DateTime utcNow, string editorUser)
+        {
+            object entity = entry.Entity;
+
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    var creationAuditable = entity as ICreationAuditable;
+                    if (creationAuditable != null)
+                    {
+                        creationAuditable.CreatorUser = editorUser;
+                    }
+                    break;
+
+                case EntityState.Modified:
+                    var modificationAuditable = entity as IModificationAuditable;
+                    if (modificationAuditable != null)
+                    {
+                        modificationAuditable.UpdaterUser = editorUser;
+                    }
+                    break;
+
+                case EntityState.Deleted:
+                    var deletionAuditable = entity as IDeletionAuditable;
+                    if (deletionAuditable != null)
+                    {
+                        deletionAuditable.DeleterUser = editorUser;
+                    }
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            UpdateTrackableEntity(entry, utcNow);
+        }
+
+        /// <remarks>
+        /// EF automatically detects if byde[] RowVersion is changed by reference (not only by value)
+        /// and gentrates code like 'DECLARE @p int; UPDATE [Table] SET @p = 0 WHERE RowWersion = ...'
+        /// </remarks>
+        private static void UpdateTrackableEntity(DbEntityEntry entry, DateTime utcNow)
+        {
+            object entity = entry.Entity;
+
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    var creationTrackable = entity as ICreationTrackable;
+                    if (creationTrackable != null)
+                    {
+                        creationTrackable.CreatedUtc = utcNow;
+                    }
+                    break;
+
+                case EntityState.Modified:
+                    var modificatonTrackable = entity as IModificationTrackable;
+                    if (modificatonTrackable != null)
+                    {
+                        modificatonTrackable.UpdatedUtc = utcNow;
+                    }
+                    break;
+
+                case EntityState.Deleted:
+                    var softDeletable = entity as ISoftDeletable;
+                    if (softDeletable != null)
+                    {
+                        var deletionTrackable = entry as IDeletionTrackable;
+                        if (deletionTrackable != null)
+                        {
+                            deletionTrackable.DeletedUtc = utcNow;
+                        }
+
+                        softDeletable.IsDeleted = true;
+                        entry.State = EntityState.Modified;
+                    }
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            if ((entry.State & (EntityState.Modified | EntityState.Deleted)) != 0)
+            {
+                var optimisticConcurrent = entity as IOptimisticConcurrent;
+                if (optimisticConcurrent != null)
+                {
+                    // take row version from entity that modified by client
+                    entry.Property("RowVersion").OriginalValue = optimisticConcurrent.RowVersion;
+                }
+            }
+        }
+    }
+}
