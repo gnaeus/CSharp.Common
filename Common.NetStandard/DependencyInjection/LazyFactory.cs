b6ee2190e53ce344a4ec5bdf4d6c82e2736cbb40
PanyushkinD <PanyushkinD@quantumart.ru>
2017-07-21 11:19:25 +0300

    Add Lazy and Factory for Microsoft.Extensions.DependencyInjection

---
+++ Common.NetStandard/DependencyInjection/LazyFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public interface ILazy<out T>
+    {
+        T Value { get; }
+    }
+
+    public interface IFactory<out T>
+    {
+        T Create();
+    }
+
+    internal class LazyService<T> : Lazy<T>, ILazy<T>
+    {
+        public LazyService(IServiceProvider provider)
+            : base(() => provider.GetRequiredService<T>()) { }
+    }
+    
+    public class Factory<T> : IFactory<T>
+    {
+        private readonly IServiceProvider _provider;
+
+        public Factory(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public T Create()
+        {
+            return _provider.GetRequiredService<T>();
+        }
+    }
+
+    public static class LazyFactoryExtensions
+    {
+        public static void AddLazy(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(Lazy<>), typeof(LazyService<>));
+            services.AddTransient(typeof(ILazy<>), typeof(LazyService<>));
+        }
+
+        public static void AddFactory(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(Factory<>));
+            services.AddTransient(typeof(IFactory<>), typeof(Factory<>));
+        }
+
+        public static void AddLazyFactory(this IServiceCollection services)
+        {
+            services.AddLazy();
+            services.AddFactory();
+        }
+    }
+}
---
+++ Common.NetStandard/DependencyInjection/_Examples.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+partial class _Examples
+{
+    class Program
+    {
+        static void EntryPoint()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            IServiceProvider provider = services.BuildServiceProvider();
+
+            using (IServiceScope scope = provider.CreateScope())
+            {
+                var application = scope.ServiceProvider.GetRequiredService<Application>();
+
+                application.Execute();
+            }
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddLazy();
+            // services.AddFactory();
+            services.AddLazyFactory();
+            
+            services.AddTransient<Application>();
+            services.AddTransient<Service>();
+            services.AddTransient<NativeResource>();
+
+            services.AddScoped<Reporitory>();
+        }
+    }
+
+    // [Console]
+    // Service Created
+    // Application Created
+    // NativeResource Created
+    // Reporitory Created
+    // NativeResource Disposed
+    // Application.Execute Executed
+    // NativeResource Disposed
+    // Reporitory Disposed
+
+    class Application
+    {
+        readonly Service _service;
+        readonly ILazy<Reporitory> _repository;
+        readonly IFactory<NativeResource> _nativeResourceFactory;
+
+        public Application(
+            Service service,
+            ILazy<Reporitory> repository,
+            IFactory<NativeResource> nativeResourceFactory)
+        {
+            _service = service;
+            _repository = repository;
+            _nativeResourceFactory = nativeResourceFactory;
+
+            Console.WriteLine($"{GetType().Name} Created");
+        }
+
+        public void Execute()
+        {
+            using (_nativeResourceFactory.Create())
+            {
+                var repository = _repository.Value;
+            }
+
+            Console.WriteLine($"{GetType().Name}.{nameof(Execute)} Executed");
+        }
+    }
+
+    class Service
+    {
+        public Service()
+        {
+            Console.WriteLine($"{GetType().Name} Created");
+        }
+    }
+
+    class Reporitory : IDisposable
+    {
+        public Reporitory()
+        {
+            Console.WriteLine($"{GetType().Name} Created");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{GetType().Name} Disposed");
+        }
+    }
+
+    class NativeResource : IDisposable
+    {
+        public NativeResource()
+        {
+            Console.WriteLine($"{GetType().Name} Created");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{GetType().Name} Disposed");
+        }
+    }
+}
